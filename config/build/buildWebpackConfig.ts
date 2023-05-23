bd739950c78c34aa29dd2e8dd2485438c2b7c6ac
sana5695 <deepbeatx@gmail.com>
2023-05-23 12:43:44 +0500

    settings webpack 0.2

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -8,7 +8,7 @@ import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration{
 
-    const {paths, mode} = options;
+    const {paths, mode, isDev} = options;
 
 
     return {
@@ -24,8 +24,8 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         rules: buildLoaders(),
     },
         resolve: buildResolvers() ,
-        devtool:'inline-source-map',
-        devServer: buildDevServer(options),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
 
     }
 }
--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -7,6 +7,11 @@ export interface BuildPaths {
 
 }
 
+export interface BuildEnv {
+    mode: BuildMode;
+    port: number;
+}
+
 export interface BuildOptions {
     mode: BuildMode;
     paths: BuildPaths;
