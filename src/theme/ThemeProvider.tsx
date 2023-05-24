fc4c3bc7d4fd3418ef23c0ac8a76931589ca319c
sana5695 <deepbeatx@gmail.com>
2023-05-24 22:11:24 +0500

    add toggle themes, create hook useTheme

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+import React, {FC, useMemo, useState} from 'react';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+
+const ThemeProvider:FC = ({children}) => {
+
+    const deafultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+
+    const [theme, setTheme] = useState<Theme>(deafultTheme)
+
+
+    const deafultProps = useMemo(() => ({
+            theme: theme,
+            setTheme: setTheme}),
+        [theme])
+
+    return (
+        <ThemeContext.Provider value={deafultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
