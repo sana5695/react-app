c81dd92e0cea13f5dbfc3f8595f903e613546a4d
sana5695 <deepbeatx@gmail.com>
2023-06-05 23:58:59 +0500

    add ThemeSwitcher shared component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,13 +11,14 @@ import {Navbar} from "widgets/Navbar/";
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme] )}>
 
             <Navbar />
 
-            <button onClick={toggleTheme}>TOGGLE</button>
+
+
 
             <AppRouter />
 
